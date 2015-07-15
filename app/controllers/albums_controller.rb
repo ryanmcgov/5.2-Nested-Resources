985957c0dbafb29ce364379c34c548a80b601bcd
eastcoastsb <ryanmcgovern@Ryans-MacBook-Air.local>
2015-07-14 20:38:53 -0400

    albums/songs controllers

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,3 +1,54 @@
 class AlbumsController < ArtistsController
-  
+
+  def new 
+    @artist = get_artist
+    @album = @artist.albums.new 
+  end
+
+  def create 
+    @artist = get_artist
+    @album = @artist.albums.new(album_params)
+    if @album.save
+      redirect_to artist_path(@artist)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @artist = get_artist
+    @album = get_album(@artist)
+  end
+
+  def update
+    @artist = get_artist
+    @album = get_album(@artist)
+    if @album.update_attributes(album_params)
+      redirect_to artist_path(@artist)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @artist = get_artist
+    @album = get_album(@artist)
+    @album.destroy
+    redirect_to artist_path(@artist)
+  end
+
+
+  private
+
+  def get_artist
+    Artist.find(params[:artist_id])
+  end 
+
+  def get_album
+    Artist.album.find(params[:id])
+  end
+
+  def album_params
+    params.require(:album).permit(:title, :artist_id)
+  end
 end
